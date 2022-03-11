f00b109990069eb5b9811706d2f712edf3301159
Jorge <salazar107991@insuco.edu.mx>
2022-03-11 09:46:53 -0600

    resuelto

--- examen.js
+++ examen.js
@@ -1,1 +1,24 @@
 //Prueba
+//Se declarara 3 variables y van a declarar una ultima variable que sume sus primeras dos variables despues se multiplicara pero se tiene que dar porioridad a la suma
+
+let a = 15, b = 20, c = 30
+
+let z = ( a + b ) * c;
+
+console.log( z );
+
+//Declarar una variable nueva y sera variable d se le dara un tipo de dato string (numero) y se usara el comparador distinto a y se va a comparar la primera variable con la variable d
+
+let d = "10";
+
+z = a != d;
+
+console.log( z );
+
+//Vas a preguntar si tu variable b es igual o mayor a tu variable c
+
+z = b >= c;
+
+console.log( z );
+
+//
